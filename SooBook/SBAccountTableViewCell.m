657ff8a9e9a3919c67a8de172898ee406b3854a4
Chanwoong Park <foodi1121@gmail.com>
2017-04-06 21:02:26 +0900

    Add CellReceive in SBMainCollectionViewController

--- SooBook/SBAccountTableViewCell.m
+++ SooBook/SBAccountTableViewCell.m
@@ -15,7 +15,8 @@
     self.selectionStyle = UITableViewCellSelectionStyleNone;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
